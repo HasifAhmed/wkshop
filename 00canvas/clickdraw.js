5d41e2b5e45ae543295c13ed83fd9a9764818735
unknown <hahmed@stuy.edu>
2019-01-30 16:40:07 -0800

    did hw minutes done

--- 00canvas/clickdraw.js
+++ 00canvas/clickdraw.js
@@ -3,12 +3,26 @@ var c = document.getElementById("slate");
 //instantiate a CanvasRenderingContext2D
 var ctx = c.getContext("2d");
 //invoke interface methods
-document.getElementById("Create").addEventListener("click", function() {
-  ctx.fillStyle = "#ff0000";  //make rectangle red
-  ctx.fillRect(50, 50, 100, 200); //fill rectangle
+document.getElementById("Toggle").addEventListener("click", function() {
+  if (document.getElementById("Toggle").innerHTML =="Draw-A-Rectangle"){
+    document.getElementById("Toggle").innerHTML = "Draw-A-Dot";
+  }
+  else
+  {
+    document.getElementById("Toggle").innerHTML = "Draw-A-Rectangle";
+  }
+});
+
+document.getElementById("slate").addEventListener("click", function(ev) {
+  if (document.getElementById("Toggle").innerHTML =="Draw-A-Rectangle"){
+    ctx.fillRect(ev.clientX - c.offsetLeft, ev.clientY - c.offsetTop, 100, 200);
+  }
+  else if(document.getElementById("Toggle").innerHTML == "Draw-A-Dot"){
+    ctx.fillRect(ev.clientX - c.offsetLeft, ev.clientY - c.offsetTop, 5, 5);
+  }
 });
 
 document.getElementById("Clear").addEventListener("click", function() {
-  ctx.clearRect(50, 50, 100, 200); //clear rectangle
+  ctx.clearRect(0, 0, c.width, c.height); //clear rectangle
 
 });
