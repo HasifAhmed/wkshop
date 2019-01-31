1f4c106ed98e5eedf5681ec3675d64a729cc2504
hasifahmed <hahmed@stuy.edu>
2019-01-31 14:07:39 -0500

    oopsie

---
+++ 01canvas/clickdraw.js
@@ -0,0 +1,33 @@
+//retrieve node in DOM via ID
+var shape = "rect";
+var c = document.getElementById("slate");
+//instantiate a CanvasRenderingContext2D
+var ctx = c.getContext("2d");
+//invoke interface methods
+function changeShape() {
+  if (shape == "rect"){
+    shape = "dot";
+  }
+  else
+  {
+    shape = "rect"
+  }
+}
+
+function Draw(ev){
+  
+  if (shape == "rect"){
+    ctx.fillRect(ev.offsetX , ev.offsetY, 100, 200);
+  }
+  else if(shape == "dot"){
+    ctx.beginPath(); //resets or else ellipses are connected
+    ctx.ellipse(ev.offsetX , ev.offsetY,  10, 10,0,0,7);
+    ctx.stroke(); //the outside ring
+    ctx.fill(); //inside fill up
+  }
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, c.width, c.height); //clear rectangle
+
+}
