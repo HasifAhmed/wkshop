44784f36f1ed54737cdc7126594eae88c949d11f
hasifahmed <hahmed@stuy.edu>
2019-02-06 13:33:13 -0500

    hello hello

--- 03canvas-anim/animate.js
+++ 03canvas-anim/animate.js
@@ -14,30 +14,35 @@ var requestID;
 
 var radius = 1;
 
+var draw = function(e){
+    if (requestID == null)
+    {
+      drawDot();
+    }
+    else
+    {
+      e.preventDefault();
+      console.log("Nope");																//resets value of requestID to null
+    }
+}
 var drawDot = function() {
-    if (requestID != null)
-	{
-		e.preventDefault();
-		console.log("Animation Ocurring!");
-	}
-	else {
+
 	if(radius>=c.width/2 || radius <= 0){
 		growing=!growing;
 	}
 	if( growing ) {
-		radius += 1; 
+		radius += 1;
 	} else {
 		radius -= 1;
 	}
 	ctx.clearRect(0, 0, c.width, c.height);
 	ctx.fillStyle = "#00FFFF";
 	ctx.beginPath();
-	window.requestAnimationFrame();
 	ctx.ellipse(c.width / 2, c.height / 2, radius, radius, 0, 0, 2 * Math.PI);
 	ctx.stroke();
 	ctx.fill();
 	requestID = window.requestAnimationFrame(drawDot);
-	}
+
 
 }
 
@@ -45,16 +50,16 @@ var stopIt = function() {
     if (requestID == null)
 	{
 		e.preventDefault();																//prevents default action of the stop mechanism from running
-		console.log("Shouldn't do anything");			
+		console.log("Shouldn't do anything");
 	}
 	else
 	{
-		console.log("request ID:" + requestID );		
+		console.log("request ID:" + requestID );
 		window.cancelAnimationFrame(requestID);											//cancels the animation frame
 		requestID = null;																//resets value of requestID to null
 	}
-    
+
 }
 
-dotButton.addEventListener("click", drawDot);
-stopButton.addEventListener("click", stopIt); 
+dotButton.addEventListener("click", draw);
+stopButton.addEventListener("click", stopIt);
