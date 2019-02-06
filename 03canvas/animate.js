8711eb61b0ade90848f2049769cfa79041ca2dee
Hasif Ahmed <33489092+HasifAhmed@users.noreply.github.com>
2019-02-06 04:03:41 -0800

    Update animate.js

--- 03canvas/animate.js
+++ 03canvas/animate.js
@@ -4,34 +4,46 @@ var ctx = c.getContext("2d");
 var shape = "circle";
 
 var stopButton  = document.getElementById("stop");
-stopButton.addEventListener("click", stopIt); 
+
 var dotButton = document.getElementById("circle");
-dotButton.addEventListener("click", drawDot);
+
 
 var growing = true;
 
-var requestID;
+var requestID = false;
 
 var radius = 10;
 
 var drawDot = function() {
-    
-    ctx.beginPath();
-    if( growing ) {
-	radius += 1; 
-    } else {
-	radius -= 1;
+    if(requestID){
+	    stopIt();
     }
-    window.requestAnimationFrame();
-    ctx.arc( c.width / 2, c.height / 2, radius,  0, 2*Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    
+	clear;
+	ctx.fillStyle = "#00FFFF";
+	ctx.beginPath();
+	if(radius==0 || radius==c.height/2 || radius==c.width/2){
+		growing=!growing;
+	}
+	if( growing ) {
+		radius += 1; 
+	} else {
+		radius -= 1;
+	}
+	window.requestAnimationFrame();
+	ctx.arc( c.width / 2, c.height / 2, radius,  0, 2*Math.PI);
+	ctx.stroke();
+	ctx.fill();
+	requestID = window.requestAnimationFrame(drawDot);
+	
+
 }
 
 var stopIt = function() {
     console.log( requestID );
-    
+    window.cancelAnimationFrame(requestID);
+    requestID=false;
     
 }
 
+dotButton.addEventListener("click", drawDot);
+stopButton.addEventListener("click", stopIt); 
