3ad72b5521c390c0e94b09735b3cf97ee70a3f18
Hasif Ahmed <33489092+HasifAhmed@users.noreply.github.com>
2019-03-04 19:35:29 -0800

    Update foodmongo.py

--- 06_mongo/foodmongo.py
+++ 06_mongo/foodmongo.py
@@ -10,27 +10,27 @@ connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.test
 collection = db.restaurants
 
-def by_borough(borough):
+def borough(borough):
     for doc in collection.find( {"borough": borough} ):
         pprint.pprint(doc)
         print('\n')
 
-def by_zip(zipcode):
+def zip(zipcode):
     for doc in collection.find( {"address.zipcode": zipcode} ):
         pprint.pprint(doc)
         print('\n')
 
-def by_zip_and_grade(zipcode, grade):
+def zip_and_grade(zipcode, grade):
     for doc in collection.find( { '$and': [ {"address.zipcode": zipcode}, {"grades.grade": grade} ] } ):
         pprint.pprint(doc)
         print('\n')
 
-def by_score_threshold(score):
+def score_threshold(score):
     for doc in collection.find( {"grades.score": {'$lt': score} } ):
         pprint.pprint(doc)
         print('\n')
 
-def by_cui_and_street(cuisine, street): 
-    for doc in collection.find( { '$and': [ {"cuisine": cuisine}, {"address.street": street} ] } ):
+def cuisine_and_score(cuisine, score): 
+    for doc in collection.find( { '$and': [ {"cuisine": cuisine}, {'grades.0.score':{'$gt':score}} ] } ):
         pprint.pprint(doc)
         print('\n')
