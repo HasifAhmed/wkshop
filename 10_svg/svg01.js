14c78417d7173b3523b8e1973cfe15154db4744b
unknown <hahmed@stuy.edu>
2019-03-13 20:45:34 -0800

    bois

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,66 +1,84 @@
 /*
 Hasif Ahmed
-SoftDev2 pd6
-K#09 -- Connect the Dots
-2019-03-13
+SoftDev2 pd8
+K#10 -- Ask Circles [Change || Die]
+2019-03-14
 */
 
 var pic = document.getElementById("vimage");
 
-var previous = 0;
-
 var drawDot = function(e){
-  //first dot no line
-  /*if (previous != 0)
-  {
-      var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
+    var children = pic.children;
+    var blue = false;
 
-      //making the line
-      line.setAttribute("x1", previous.getAttribute( "cx" ) );
-      line.setAttribute("y1", previous.getAttribute("cy") );
-      line.setAttribute("x2", e.offsetX);
-      line.setAttribute("y2", e.offsetY);
-      line.setAttribute("stroke", "black");
+    for(child of children){
 
-      //moves curr pos to new pos
-      pic.appendChild(line);
-
-    }
-    */
-    if(pic.hasChildNodes()){
-      var i;
-      for (i = 0; i < cars.length; i++) {
-        text += cars[i] + "<br>";
+      if( inBounds(child,e)){
+        makeBlue(child);
+        blue = true;
+        break;
+      }
+      else {
+        blue = false;
       }
     }
-    else{
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
-    //creating the dot
-    c.setAttribute("cx", e.offsetX);
-    c.setAttribute("cy", e.offsetY);
-    c.setAttribute("r", 10);
-    c.setAttribute("fill", "red");
-    c.setAttribute("stroke", "black");
-
-    pic.appendChild(c);
+    if (!(blue)) {
+      pic.appendChild(createCircle(e.offsetX, e.offsetY));
+    }
 
 
-    previous = c;
-  }
 };
 
 
 var clear = function(){
-  //reloads page - resets the board
   while (pic.hasChildNodes()){
-		pic.removeChild(pic.lastChild)
-		oldX=undefined;
-		oldY=undefined;
+		pic.removeChild(pic.lastChild);
+
   }
-  //previous = 0;
+
+
 };
+
+// Check if inbounds using d^2 < r^2
+var inBounds = function(circle, event){
+
+ //circle coords
+ var cx = circle.getAttribute("cx")
+ var cy = circle.getAttribute("cy")
+
+ // point coords
+ var px = event.offsetX
+ var py = event.offsetY
+
+ var r = circle.getAttribute("r")
+
+ return Math.pow( (px-cx), 2) + Math.pow( (py-cy), 2) < Math.pow(r,2)
+}
+
+var createCircle = function(x,y){
+  var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  c.setAttribute("cx", x);
+  c.setAttribute("cy", y);
+  c.setAttribute("r", 10);
+  c.setAttribute("fill", "red");
+  c.setAttribute("stroke", "black");
+  return c;
+
+}
+
+var makeBlue = function(circle){
+  if(circle.getAttribute("fill") == "red"){
+    circle.setAttribute("fill", "blue")
+  }
+  else{
+    //new circle at random loc
+    var rand = createCircle(Math.random() * 500, Math.random() * 500)
+    pic.appendChild(rand)
+    pic.removeChild(circle)
+  }
+}
 
 pic.addEventListener("click",drawDot);
 document.getElementById("but_clear").addEventListener("click", clear);
