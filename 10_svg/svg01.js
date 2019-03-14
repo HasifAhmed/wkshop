7c3be6c008ccb3a9fa7a8424bf0709ec0aeae798
hasifahmed <hahmed@stuy.edu>
2019-03-14 14:01:08 -0400

    fixed

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -7,78 +7,55 @@ K#10 -- Ask Circles [Change || Die]
 
 var pic = document.getElementById("vimage");
 
-var drawDot = function(e){
+const radius = 10;
 
-    var children = pic.children;
-    var blue = false;
+//kills off children
+document.getElementById('but_clear').addEventListener('click', function(e)
+	{
+	while (pic.hasChildNodes()){
+		pic.removeChild(pic.lastChild)
+	}
+	});
 
-    for(child of children){
-
-      if( inBounds(child,e)){
-        makeBlue(child);
-        blue = true;
-        break;
-      }
-      else {
-        blue = false;
-      }
-    }
-
-    if (!(blue)) {
-      pic.appendChild(createCircle(e.offsetX, e.offsetY));
-    }
-
-
-};
-
-
-var clear = function(){
-  while (pic.hasChildNodes()){
-		pic.removeChild(pic.lastChild);
-
-  }
-
-
-};
-
-// Check if inbounds using d^2 < r^2
-var inBounds = function(circle, event){
-
- //circle coords
- var cx = circle.getAttribute("cx")
- var cy = circle.getAttribute("cy")
-
- // point coords
- var px = event.offsetX
- var py = event.offsetY
-
- var r = circle.getAttribute("r")
-
- return Math.pow( (px-cx), 2) + Math.pow( (py-cy), 2) < Math.pow(r,2)
+//returns mouse coords
+function mouseCoords(e)
+{
+	return { x: e.offsetX, y: e.offsetY };
 }
 
-var createCircle = function(x,y){
-  var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  c.setAttribute("cx", x);
-  c.setAttribute("cy", y);
-  c.setAttribute("r", 10);
-  c.setAttribute("fill", "red");
-  c.setAttribute("stroke", "black");
-  return c;
-
+//randomizes numbers - useful for the random coords
+function random(x)
+{
+	return Math.floor(Math.random()*x)
 }
 
-var makeBlue = function(circle){
-  if(circle.getAttribute("fill") == "red"){
-    circle.setAttribute("fill", "blue")
-  }
-  else{
-    //new circle at random loc
-    var rand = createCircle(Math.random() * 500, Math.random() * 500)
-    pic.appendChild(rand)
-    pic.removeChild(circle)
-  }
-}
+//if mouse coords have no color -> make circle
+//if mouse coords have color of red -> make circle blue
+//if mouse coords have color of blue -> new circle random coords
+pic.addEventListener('click', function(e)
+	{
+		color=e.target.getAttribute("fill");
+		var mousePos = mouseCoords(e);
 
-pic.addEventListener("click",drawDot);
-document.getElementById("but_clear").addEventListener("click", clear);
+    //if empty
+		if(color == null){
+			var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+			c.setAttribute("cx", mousePos.x);
+			c.setAttribute("cy", mousePos.y);
+			c.setAttribute("r", radius);
+			c.setAttribute("fill", "red");
+			c.setAttribute("stroke", "black");
+			pic.appendChild(c);
+			a=pic.childNodes
+		}
+		//if red
+		else if(color == "red"){
+			e.target.setAttribute("fill","blue");
+		}
+		//if blue
+		else{
+			e.target.setAttribute("cx",random(501));
+			e.target.setAttribute("cy",random(501));
+			e.target.setAttribute("fill","red");
+		}
+});
