4ce1483b5d0dc1112ddf6640c7b9be78df194c29
hasifahmed <hahmed@stuy.edu>
2019-04-30 14:07:13 -0400

    ok

--- dk.py
+++ dk.py
@@ -1,13 +1,22 @@
 def outer(x):
 	def contains(l):
-		return x in l
-	return contains
+		return x * l
+	return contains 
 
-contains_15 = outer(15)
 
-print(contains_15(range(1,20)))
+contains_15 = outer('cool')
+
+print(contains_15(3))
 
 #print(outer(15)(range(1,20)))
 
-del(outer)
-print(outer(42))
+#del(outer)
+#print(outer(42))
+
+def make_counter():
+	def add():
+		return make_counter + 1
+	return make_counter
+
+ctr1 = make_counter(
+print(ctr1())
